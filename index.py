9db63436209d744f4d6ac97c3223d2e3ede85a50
abhishekwl <nateriver210@gmail.com>
2019-02-17 08:29:19 +0530

    Added support for 4 categories

--- index.py
+++ index.py
@@ -11,7 +11,12 @@ auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 extractor = tweepy.API(auth)
 
-all_tweets = extractor.search(q=QUERY, count=200)
+print('\n\nStrongly for(FF)')
+print('\nFor(F)')
+print('\nAgainst(A)')
+print('\nStrongly against(AA)\n\n')
+
+all_tweets = extractor.search(q=QUERY, count=300)
 with open('dataset.csv', 'w') as file:
     for tweet in all_tweets:
         tweet = tweet._json
@@ -21,12 +26,13 @@ with open('dataset.csv', 'w') as file:
         tweet_sentiment_subjectivity = tweet_wiki.sentiment.subjectivity
         tweet_sentiment_polarity = tweet_wiki.sentiment.polarity
         tweet_retweet_count = int(tweet["retweet_count"])
+        tweet_author_followers_count = int(tweet["user"]["followers_count"])
 
-        print(tweet_text_content+" (F or A) : ")
+        print(tweet_text_content+" (FF or F or A or AA) : ")
         label = input()
         label = label.upper()
         
-        file.write("{},{},{},{}\n".format(tweet_sentiment_polarity,tweet_sentiment_subjectivity,tweet_retweet_count,label))
+        file.write("{},{},{},{},{}\n".format(tweet_sentiment_polarity,tweet_sentiment_subjectivity, tweet_author_followers_count, tweet_retweet_count,label))
     file.close()
 
 print('Shoo now')
